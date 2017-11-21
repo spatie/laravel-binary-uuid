b7e7e512d86fea9a0b540980b1552f0a9e1829e0
Brent Roose <brent@spatie.be>
2017-11-21 09:32:26 +0100

    Support SQLiteGrammar

--- src/UuidServiceProvider.php
+++ src/UuidServiceProvider.php
@@ -2,7 +2,12 @@
 
 namespace Spatie\BinaryUuid;
 
+use Exception;
+use Illuminate\Database\Connection;
+use Illuminate\Database\Schema\Grammars\Grammar;
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Database\Query\Grammars\MySqlGrammar as IlluminateMySqlGrammar;
+use Illuminate\Database\Query\Grammars\SQLiteGrammar as IlluminateSQLiteGrammar;
 
 class UuidServiceProvider extends ServiceProvider
 {
@@ -11,6 +16,24 @@ class UuidServiceProvider extends ServiceProvider
         /** @var \Illuminate\Database\Connection $connection */
         $connection = app('db')->connection();
 
-        $connection->setSchemaGrammar(new MySqlGrammar());
+        $connection->setSchemaGrammar($this->createGrammarFromConnection($connection));
+    }
+
+    protected function createGrammarFromConnection(Connection $connection): Grammar
+    {
+        $queryGrammar = $connection->getQueryGrammar();
+
+        if (!in_array(
+            get_class($queryGrammar),
+            [IlluminateMySqlGrammar::class, IlluminateSQLiteGrammar::class]
+        )) {
+            throw new Exception("spatie/laravel-binary-uuid only supports MySql or SQLite connections. The current connection doesn't match those criteria.");
+        }
+
+        if ($queryGrammar instanceof IlluminateSQLiteGrammar) {
+            return new SQLiteGrammar();
+        }
+
+        return new MySqlGrammar();
     }
 }
