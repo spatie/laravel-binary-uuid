a7e90c50596e4631c92e5b21166bbac3b0ca9c61
freek <freek@spatie.be>
2017-11-29 14:08:02 +0100

    refactor to collections

--- tests/Benchmark/BenchmarkTest.php
+++ tests/Benchmark/BenchmarkTest.php
@@ -2,6 +2,7 @@
 
 namespace Spatie\BinaryUuid\Test\Benchmark;
 
+use Illuminate\Support\Collection;
 use PHPUnit\Framework\TestCase;
 use Doctrine\DBAL\Configuration;
 use Doctrine\DBAL\DriverManager;
@@ -30,14 +31,14 @@ class BenchmarkTest extends TestCase
     /** @test */
     public function run_benchmarks()
     {
-        $benchmarks = [
+        $benchmarks = collect([
             new NormalId($this->connection),
             new BinaryUuid($this->connection),
             new OptimisedUuid($this->connection),
             new OptimisedUuidFromText($this->connection),
             (new TextualUuid($this->connection))
                 ->withBenchmarkRoundsTextualUuid(getenv('BENCHMARK_ROUNDS_TEXTUAL_UUID')),
-        ];
+        ]);
 
         $this->writeln('Starting benchmarks...');
 
@@ -63,40 +64,41 @@ class BenchmarkTest extends TestCase
         });
     }
 
-    protected function runIteration(array $benchmarks, int $recordsInTable)
+    protected function runIteration(Collection $benchmarks, int $recordsInTable)
     {
-        foreach ($benchmarks as $benchmark) {
-            $benchmark
-                ->withRecordsInTable($recordsInTable)
-                ->withBenchmarkRounds(getenv('BENCHMARK_ROUNDS'))
-                ->withFlushAmount(getenv('FLUSH_QUERY_AMOUNT'));
-        }
+        $benchmarks
+            ->each(function ($benchmark) use ($recordsInTable) {
+                $benchmark
+                    ->withRecordsInTable($recordsInTable)
+                    ->withBenchmarkRounds(getenv('BENCHMARK_ROUNDS'))
+                    ->withFlushAmount(getenv('FLUSH_QUERY_AMOUNT'));
+            });
 
         $this->writeln("\nCreating tables");
 
-        foreach ($benchmarks as $benchmark) {
+        $benchmarks->each(function (Benchmark $benchmark) {
             $benchmark->createTable();
 
             $this->writeln("\t- {$benchmark->name()}");
-        }
+        });
 
         $this->writeln("\nSeeding tables");
 
-        foreach ($benchmarks as $benchmark) {
+        $benchmarks->each(function (Benchmark $benchmark) {
             $benchmark->seedTable();
 
             $this->writeln("\t- {$benchmark->name()}");
-        }
+        });
 
         $this->writeln("\nRunning benchmarks");
 
-        foreach ($benchmarks as $benchmark) {
+        $benchmarks->each(function (Benchmark $benchmark) {
             $this->writeln("\t- {$benchmark->name()}: ");
 
             $result = $benchmark->run();
 
             $this->writeln("\t\tAvarage of {$result->getAverageInMilliSeconds()}ms over {$result->getIterations()} iterations.");
-        }
+        });
     }
 
     protected function writeln(string $message)
