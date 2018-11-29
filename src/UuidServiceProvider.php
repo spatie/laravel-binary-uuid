5e9dae8f16fa458b37fbaf80c13450f8bc7b0158
Blake Patton <blake@surg.io>
2018-11-29 13:40:25 -0700

    Remove Blueprint grammar

--- src/UuidServiceProvider.php
+++ src/UuidServiceProvider.php
@@ -16,36 +16,8 @@ class UuidServiceProvider extends ServiceProvider
 {
     public function boot()
     {
-        /** @var \Illuminate\Database\Connection $connection */
-        $connection = app('db')->connection();
-
-        $connection->setSchemaGrammar($this->createGrammarFromConnection($connection));
-
+        
         $this->optimizeUuids();
-    }
-
-    protected function createGrammarFromConnection(Connection $connection): Grammar
-    {
-        $queryGrammar = $connection->getQueryGrammar();
-
-        $queryGrammarClass = get_class($queryGrammar);
-
-        if (! in_array($queryGrammarClass, [
-            IlluminateMySqlGrammar::class,
-            IlluminateSQLiteGrammar::class,
-        ])) {
-            throw new Exception("There current grammar `$queryGrammarClass` doesn't support binary uuids. Only  MySql and SQLite connections are supported.");
-        }
-
-        if ($queryGrammar instanceof IlluminateSQLiteGrammar) {
-            $grammar = new SQLiteGrammar();
-        } else {
-            $grammar = new MySqlGrammar();
-        }
-
-        $grammar->setTablePrefix($queryGrammar->getTablePrefix());
-
-        return $grammar;
     }
 
     protected function optimizeUuids()
